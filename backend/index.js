41d2d33b84067028c11703ab6bed44ba5b4effc3
shivamkumar2005-abes <shivamprajapati1704@gmail.com>
2026-08-09 12:44:53 +0530

    Initial commit - ShopNest MERN e-commerce app

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/authroutes.js");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("shopnest backend is working properly ");
+});
+
+app.use('/api/auth',require('./routes/authroutes'));
+app.use('/api/products',require('./routes/productRoutes'));
+app.use('/api/orders',require('./routes/orderRoutes'));
+app.use('/api/payment',require('./routes/paymentRoutes'));
+app.use('/api/analytics',require('./routes/analyticsRoutes'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
